14ec75220c25ec3c8548ce41a58ef868ec5340f4
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-19 01:17:28 +0200

    6th question

--- Task_1.py
+++ Task_1.py
@@ -3,24 +3,33 @@
 x = True
 print ('x =', type(x))
 
+print ('--------------------------------')
+# ------------------------------------------------
+
 
 ## 2. Create an integer variable named y :
 
 y = 10
 print ('y =', type(y))
 
+print ('--------------------------------')
+# ------------------------------------------------
 
 ## 3. Create a float variable named z :
 
 z = 1.0
 print ('z =',type(z))
 
+print ('--------------------------------')
+# ------------------------------------------------
 
 ## 4. Create a string variable names s :
 
 s = 'Wael'
 print ('s =', type(s))
 
+print ('--------------------------------')
+# ------------------------------------------------
 
 ## 5. Convert the int variable to float :
 
@@ -28,3 +37,18 @@ p = 11
 print ('p =', type(p))
 p = float (p)
 print ('p has been converted to',type(p))
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+## 6. Can we convert the str to int ?
+
+h = '21'
+print (int(h), type(h))
+h = int = 1
+print ((1),'is now the new vlaue of h = :',type(h))
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+## 7. Create a list of numbers from 1 to 5
