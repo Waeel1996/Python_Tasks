1f7d4a1bb279d59b42442df4d8480b3cda137e2e
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-19 00:51:54 +0200

    3rd question

--- Task_1.py
+++ Task_1.py
@@ -6,5 +6,11 @@ print ('x =', type(x))
 
 ## 2. Create an integer variable named y :
 
-x = 10
-print ('x =', type(x))
+y = 10
+print ('y =', type(y))
+
+
+## 3. Create a float variable named z
+
+z = 1.0
+print ('z =',type(z))
