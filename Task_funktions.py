08ca04bd94381696d75cadc92563f7fa29adcd3b
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-20 00:29:29 +0200

    funktions tasks

---
+++ Task_funktions.py
@@ -0,0 +1,10 @@
+# 1. Create a simple function that takes 2 numbers and print their values :
+
+def func(x,y):
+    print(x,y)
+func(1,2)
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
