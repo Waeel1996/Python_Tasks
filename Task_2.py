93e9f73b84b45d66cf851932119a22c0bd421df3
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-19 02:24:06 +0200

    task_2 q4

--- Task_2.py
+++ Task_2.py
@@ -4,8 +4,6 @@ print ('The = is a simple assignment operator. It assigns values from right side
 
 
 print ('--------------------------------')
-
-
 # ------------------------------------------------
 
 # 2. What do we mean by using !=
@@ -13,6 +11,7 @@ print ('--------------------------------')
 print("The not-equal-to operator ( != ) returns true if the operands don't have the same value; otherwise, it returns false .")
 
 
+print ('--------------------------------')
 # ------------------------------------------------
 
 # 3. What is the operator precedence
@@ -20,6 +19,16 @@ print("The not-equal-to operator ( != ) returns true if the operands don't have 
 print ("Operator precedence in Python simply refers to the order of operations. Operators are used to perform operations on variables and values. Python classifies its operators in the following groups: Arithmetic operators. Assignment operators.")
 
 
+print ('--------------------------------')
 # ------------------------------------------------
 
 
+# 4. Create a variable x with value of 10
+
+
+x = 10
+print(x)
+
+
+print ('--------------------------------')
+# ------------------------------------------------
