47a4309a68dd7d6199fad78fcba8c5a4069639df
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-19 02:17:39 +0200

    task_2 q1

---
+++ Task_2.py
@@ -0,0 +1,11 @@
+## 1. What is the differences between = , ==
+
+print ('The = is a simple assignment operator. It assigns values from right side operands to the left side operand. While on the other hand == checks if the values of two operands are equal or not.')
+
+
+print ('--------------------------------')
+
+
+# ------------------------------------------------
+
+
