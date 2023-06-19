3a9ad045cf9117d18b5231155bd041303439eed2
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-19 03:19:45 +0200

    task_3 q1 and 2

---
+++ Task_3.py
@@ -0,0 +1,11 @@
+# 1. Check if 10 is bigger than 15 or not
+# 2. If 10 is not bigger than15 print x is smaller than 15
+
+x = 10
+if x > 15 :
+    print ('10 is bigger thanx 15')
+else :
+    print ('10 is smaller than 15')
+
+print ('--------------------------------')
+# ------------------------------------------------
