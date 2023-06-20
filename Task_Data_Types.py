453d7d6c73982adeac5ec2c585d009bcda5e33ee
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-20 17:21:25 +0200

    task_5

---
+++ Task_Data_Types.py
@@ -0,0 +1,131 @@
+# 1. what is the differences between ‘ ’ “ ” ‘’’ ‘’’ :
+
+print ('single and double quotes are the same. In other words, we can use single and double quotes interchangeably when we declare a string. Like other programming languages, when a string contains special characters like quotes, we need to escape them we need to escape single or double quotes depending on whatenclosing quotes the string uses then we can use the Triple Quotes.')
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 2. Create a string with the name ‘mystero’
+
+x = ('mystro')
+print ('x = ',type(x))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 3. Make the string letters capital
+
+x = ('mystro')
+print ('x =',x.upper())
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 4. Create a list of values from 10 to 20
+
+l = [10,11,12,13,14,15,16,17,18,19,20]
+print ('l =',(l),type(l))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 5. Add 30 to the end of the list
+
+l = l = [10,11,12,13,14,15,16,17,18,19,20]
+l.append (30)
+print ('l =',(l),type(l))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 6. Add 40 as the second element of the list
+
+l = l = [10,11,12,13,14,15,16,17,18,19,20]
+l.insert (1,40)
+print ('l =',(l),type(l))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 7. Print how many elements in the list
+
+l = l = [10,11,12,13,14,15,16,17,18,19,20]
+print ('l =',len(l),type(l))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+#8. replace the second element in the list with 100
+
+l = l = [10,11,12,13,14,15,16,17,18,19,20]
+l[1]=100
+print ('l =',(l))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 9. Create a tuple with values from 1 to 5
+
+
+t = (1,2,3,4,5)
+print ('t =',(t),type(t))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 10. Can we add 10 to the end of the tuple?
+
+print ('No, becasue tuple is unchangeable')
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 11. Create a dict of value Mahmoud:28 , ahmed:30
+
+d={'Mahmoud':28 , 'ahmed':30}
+print ('d =',(d),type(d))
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 12. Print Mahmoud age from the dict
+
+d={'Mahmoud':28 , 'ahmed':30}
+
+print (d['Mahmoud'])
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
+
+# 13. What is the differences between mutable and immutable data types ?
+
+
+print("Mutable objects are those that allow you to change their value or data in place without affecting the object's identity. In contrast, immutable objects don't allow this kind of operation")
+
+
+
+print ('--------------------------------')
+# ------------------------------------------------
